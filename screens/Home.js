751fbc8ef8923882fbb4bb59abc6361e5c3c9e4c
Serveriii <n2jose00@students.oamk.fi>
2024-03-19 15:29:07 +0200

    App layout and color testing

--- screens/Home.js
+++ screens/Home.js
@@ -7,21 +7,27 @@ import { general, gradientColors } from "../styles/general";
 
 export default function Home() {
   return (
-    <LinearGradient
-      useAngle={true}
-      angle={45}
-      angleCenter={{ x: 0.5, y: 0.5 }}
-      start={{ x: 0.0, y: 0.25 }}
-      end={{ x: 0.5, y: 1.0 }}
-      colors={gradientColors}
-      style={general.scaffold}
-    >
-      <Text style={general.title}>Welcome to the Home Screen</Text>
-      <Button
+    <View style={general.scaffold}>
+      <LinearGradient
+        useAngle={true}
+        angle={45}
+        angleCenter={{ x: 0.5, y: 0.5 }}
+        start={{ x: 0.0, y: 0.25 }}
+        end={{ x: 0.5, y: 1.0 }}
+        colors={gradientColors}
+        style={general.container}
+      >
+        <Text style={general.title}>My excercise routine</Text>
+        {/* <Button
         children="Show my progress!"
         mode="contained"
         onPress={() => console.log("Button Pressed")}
-      />
-    </LinearGradient>
+      /> */}
+      </LinearGradient>
+      <View style={general.progress}>
+        <Text style={{ color: "white" }}>Milestone achieved!</Text>
+        <Text style={{ color: "white" }}>Run 10km</Text>
+      </View>
+    </View>
   );
 }
--- styles/general.js
+++ styles/general.js
@@ -1,15 +1,37 @@
 import { StyleSheet } from "react-native";
 
-export const gradientColors = ["#F03800", "#F08C01", "#F0A900"];
+export const gradientColors = ["#c36800", "#ff7d03", "#ff7b00"];
 
 export const general = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
+    color: "white",
   },
   scaffold: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#5b5b5b",
+  },
+  container: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    width: "80%",
+    borderWidth: 2,
+    borderColor: "#ffcc00",
+    borderRadius: 10,
+  },
+  progress: {
+    flex: 0.2,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    width: "80%",
+    backgroundColor: "#965e1a",
+    margin: 10,
   },
 });
