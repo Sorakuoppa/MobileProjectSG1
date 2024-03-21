4721f05e77b1e0abf6f1784fdc60cc9de647dc4a
saruot <ruotsalainen.samuli@gmail.com>
2024-03-21 12:50:48 +0200

    Fixing navigation stuff

---
+++ screens/accountManagement/Account.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { general } from '../../styles/general'
+export default function Account() {
+  return (
+    <View style={general.scaffold}>
+        <Text style={general.title}>Account </Text>
+        </View>
+    )
+}
