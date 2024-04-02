a1e3db6387c812c64a221e948bd62492a39a93ba
matti pitkänen <sepfire.m@gmail.com>
2024-04-02 13:56:57 +0300

    exercise add button functionality semi fixed

--- screens/trackingScreens/templates/Exercise.js
+++ screens/trackingScreens/templates/Exercise.js
@@ -1,3 +1,4 @@
+// Exercise.js
 import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { useTheme } from "@react-navigation/native";
@@ -15,21 +16,47 @@ export default function Exercise({ template }) {
   const pullDay = exerciseData.filter((exercise) => exercise.type === "pull");
   const legDay = exerciseData.filter((exercise) => exercise.type === "legs");
 
+  // State to manage selected exercises
+  const [selectedExercises, setSelectedExercises] = useState([]);
+
+  // Function to handle adding selected exercises
+  const handleAddTracker = () => {
+    console.log(selectedExercises); // Log selectedExercises array
+    // You can perform any other operations with selectedExercises here
+  };
+
   return (
     <View style={general.scaffold}>
       <Icon name={template.icon} size={40} color={colors.primary} />
       <Text style={{ ...general.title, color: colors.text }}>Exercise </Text>
       <ScrollView style={{ width: "80%" }}>
-        <CollapsibleComponent dataList={pushDay} title="Push day" />
-        <CollapsibleComponent dataList={pullDay} title="Pull day" />
-        <CollapsibleComponent dataList={legDay} title="Leg day" />
+        {/* Pass setSelectedExercises to CollapsibleComponent */}
+        <CollapsibleComponent
+          dataList={pushDay}
+          title="Push day"
+          selectedExercises={selectedExercises}
+          setSelectedExercises={setSelectedExercises}
+        />
+        <CollapsibleComponent
+          dataList={pullDay}
+          title="Pull day"
+          selectedExercises={selectedExercises}
+          setSelectedExercises={setSelectedExercises}
+        />
+        <CollapsibleComponent
+          dataList={legDay}
+          title="Leg day"
+          selectedExercises={selectedExercises}
+          setSelectedExercises={setSelectedExercises}
+        />
       </ScrollView>
+      {/* Button to add tracker */}
       <Button
         children="Add this tracker"
         mode="contained"
         buttonColor={colors.primary}
         style={{ marginBottom: 20 }}
-        onPress={() => console.log("Add new exercise")}
+        onPress={handleAddTracker}
       />
     </View>
   );
