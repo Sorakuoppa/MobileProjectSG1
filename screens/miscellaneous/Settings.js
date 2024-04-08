3441ec9e6025be601cf48d1feb60602ce6b5de81
Serveriii <n2jose00@students.oamk.fi>
2024-04-08 13:42:12 +0300

    Conditional drawer logo done

--- screens/miscellaneous/Settings.js
+++ screens/miscellaneous/Settings.js
@@ -1,28 +1,34 @@
-import React, {useState, useContext} from "react";
-import { ThemeContext } from "../../components/Context";
+import React, { useState, useContext } from "react";
+import { ThemeContext } from "../../components/ThemeContext";
 import { View, Text, useColorScheme } from "react-native";
 import { Switch } from "react-native-paper";
 import { useTheme } from "@react-navigation/native";
 import { general } from "../../styles/general";
-import { settingStyle}  from "../../styles/miscellaneous/settingStyle";
+import { settingStyle } from "../../styles/miscellaneous/settingStyle";
 
 export default function Settings() {
-    const [switchOn, setSwitchOn] = useState(true);
-    const { colors } = useTheme();
-    const theme = useContext(ThemeContext);
-    
-    const toggleSwitch = () => {
-        setSwitchOn(!switchOn);
-        theme.setTheme(switchOn ? "light" : "dark");
-    };
+  const [switchOn, setSwitchOn] = useState(true);
+  const { colors } = useTheme();
+  const theme = useContext(ThemeContext);
 
-    return (
-        <View style={general.scaffold}>
-            <Text style={general.title}>Settings</Text>
-            <View style={settingStyle.setting}>
-                <Text style={{...general.text, color: colors.text, fontSize: 16}}>Dark mode</Text>
-                <Switch value={switchOn} onValueChange={toggleSwitch} color={colors.primary} />
-                </View>
-        </View>
-    );
+  const toggleSwitch = () => {
+    setSwitchOn(!switchOn);
+    theme.setTheme(switchOn ? "light" : "dark");
+  };
+
+  return (
+    <View style={general.scaffold}>
+      <Text style={general.title}>Settings</Text>
+      <View style={settingStyle.setting}>
+        <Text style={{ ...general.text, color: colors.text, fontSize: 16 }}>
+          Dark mode
+        </Text>
+        <Switch
+          value={switchOn}
+          onValueChange={toggleSwitch}
+          color={colors.primary}
+        />
+      </View>
+    </View>
+  );
 }
