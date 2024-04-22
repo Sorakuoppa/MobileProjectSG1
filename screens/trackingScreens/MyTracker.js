dbb35738edb60810f77a01f96f205bf9aa787bec
Sorakuoppa <n2hyti00@students.oamk.fi>
2024-04-22 13:11:06 +0300

    working my traker -screen's delete tracker button and pop-up

--- screens/trackingScreens/MyTracker.js
+++ screens/trackingScreens/MyTracker.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Button, Alert } from "react-native";
-import { ActivityIndicator } from "react-native-paper";
+import { View, Text, Alert } from "react-native";
+import { ActivityIndicator, Button, Portal, Dialog } from "react-native-paper";
 import { useTheme } from "@react-navigation/native";
-
 import Icon from "react-native-vector-icons/FontAwesome5";
 import ProgressComponent from "./components/ProgressComponent";
 import { useLoginContext } from "../../components/Contexts/LoginContext";
@@ -12,6 +11,7 @@ import { set } from "@firebase/database";
 
 export default function MyTracker({ route, navigation }) {
   const { tracker } = route.params;
+  const [dialog, setDialog] = useState(false);
   const [progress, setProgress] = useState(0);
   const [progressAmount, setProgressAmount] = useState(
     tracker.milestones.length * 10
@@ -50,15 +50,43 @@ export default function MyTracker({ route, navigation }) {
           tracker.type === "Running"
             ? "running"
             : tracker.type === "Reading"
-            ? "book"
-            : tracker.type === "Exercise"
-            ? "dumbbell"
-            : "question"
+              ? "book"
+              : tracker.type === "Exercise"
+                ? "dumbbell"
+                : "question"
         }
         size={40}
         color={colors.primary}
       />
-      <Button title="Delete Tracker" onPress={deleteTracker} />
+      <Button
+        children="Delete tracker"
+        mode="contained"
+        buttonColor="red"
+        style={{ marginTop: 10, marginBottom: 10}}
+        onPress={() => setDialog(true)} />
+      <Portal>
+        <Dialog
+          visible={dialog}
+          onDismiss={() => setDialog(false)}
+          style={{ backgroundColor: colors.accent }}
+        >
+          <Dialog.Title style={{ color: colors.text }}>
+            Are you sure you want to delete tracker?
+          </Dialog.Title>
+          <Dialog.Actions>
+            <Button
+              children="Yes"
+              textColor={colors.primary}
+              onPress={deleteTracker}
+            />
+            <Button
+              children="No"
+              textColor={colors.primary}
+              onPress={() => setDialog(false)}
+            />
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
       <Text style={{ ...general.title, color: colors.text }}>
         {tracker.name}
       </Text>
