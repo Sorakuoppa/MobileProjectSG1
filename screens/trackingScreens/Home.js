1982ff4f12426751dbe88c18ce3d2f4af6851c31
saruot <ruotsalainen.samuli@gmail.com>
2024-04-22 12:56:30 +0300

    Fixed taking a picture and deleted unneccessary code

--- screens/trackingScreens/Home.js
+++ screens/trackingScreens/Home.js
@@ -13,22 +13,7 @@ export default function Home() {
   const { colors } = useTheme();
   return (
     <View style={[general.scaffold, { backgroundColor: colors.background }]}>
-      {/* <LinearGradient
-        useAngle={true}
-        angle={45}
-        angleCenter={{ x: 0.5, y: 0.5 }}
-        start={{ x: 0.0, y: 0.25 }}
-        end={{ x: 0.5, y: 1.0 }}
-        colors={gradientColors}
-        style={general.container}
-      >
-        <Text style={general.title}>My exercise routine</Text>
-        <Button
-        children="Show my progress!"
-        mode="contained"
-        onPress={() => console.log("Button Pressed")}
-      />
-      </LinearGradient> */}
+      
       <View style={[homeStyles.progress, { backgroundColor: colors.accent }]}>
         <Text style={[homeStyles.card, { color: colors.text }]}>Today</Text>
         <Text style={[homeStyles.card, { color: colors.text }]}>May 15th</Text>
@@ -36,7 +21,11 @@ export default function Home() {
         <Text style={[homeStyles.card, { color: colors.text }]}>Milestones 0/2 done</Text>
       </View>
       <View style={{ height: 2, backgroundColor: colors.primary }} />
+
+
       <Text style={[homeStyles.card, { color: colors.text }]}>Today's milestones</Text>
+
+
       <View style={[homeStyles.progress, { backgroundColor: colors.accent }]}>
         <Text style={[homeStyles.progressDate, { color: colors.text }]}>15.5.2024</Text>
         <Text style={[homeStyles.progressName, { color: colors.text }]}>Gardening</Text>
