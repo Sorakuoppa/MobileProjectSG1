76604171a880d2c98e6cd35b71951b058bd7180e
Sorakuoppa <n2hyti00@students.oamk.fi>
2024-04-24 12:57:52 +0300

    aboutUs data changed

--- screens/miscellaneous/data/aboutUsData.js
+++ screens/miscellaneous/data/aboutUsData.js
@@ -2,27 +2,27 @@
 export const aboutUsData = [
     {
         name: 'Timo Hyttinen',
-        title: 'CEO',
+        title: 'UI/UX Designer',
         description: 'Timo is the CEO of our company. He is responsible for the overall vision and strategy of the company. He has a background in software engineering and has a passion for technology and innovation.',
     },
     {
         name: 'Roosa Rautio',
-        title: 'CTO',
+        title: 'UI/UX Designer',
         description: 'Roosa is the CTO of our company. She is responsible for the technical direction of the company. She has a background in computer science and has a passion for building great products.',
     },
     {
         name: 'Severi Jokelainen',
-        title: 'COO',
+        title: 'Lead Developer',
         description: 'Severi is the COO of our company. He is responsible for the operations of the company. He has a background in business and has a passion for building great teams.',
     }, 
     {
         name: 'Matti Pitkänen',
-        title: 'CFO',
+        title: 'Lead Developer',
         description: 'Matti is the CFO of our company. He is responsible for the financial health of the company. He has a background in finance and has a passion for building great financial systems.',
     },
     {
         name: 'Samuli Ruotsalainen',
-        title: 'CMO',
+        title: 'Lead Developer',
         description: 'Samuli is the CMO of our company. He is responsible for the marketing and sales of the company. He has a background in marketing and has a passion for building great brands.',
     }
 ];
