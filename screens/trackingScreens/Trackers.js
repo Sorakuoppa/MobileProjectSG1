9b5fd69bdb6ac45a0f691fe3e773889a6d7efbd4
saruo <ruotsalainen.samuli@gmail.com>
2024-04-24 23:44:24 +0300

    Hiding async storage button

--- screens/trackingScreens/Trackers.js
+++ screens/trackingScreens/Trackers.js
@@ -173,13 +173,14 @@ export default function Trackers({ navigation }) {
           </Dialog>
         </Portal>
         {/* Button for testing asyncStorage */}
-       <Button
+     {  /*  <Button
           children="Show asyncStorage"
           mode="contained"
           buttonColor={colors.primary}
           onPress={showAsyncStorage}
         />
-     
+       */ }
+    
       </View> 
     );
   }
