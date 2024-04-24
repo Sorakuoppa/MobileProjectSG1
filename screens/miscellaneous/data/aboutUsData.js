df29c7b1c8e30db891cdb941bd28e892a56ec9b3
Sorakuoppa <n2hyti00@students.oamk.fi>
2024-04-24 14:03:19 +0300

    about us text changes and some UI changes

--- screens/miscellaneous/data/aboutUsData.js
+++ screens/miscellaneous/data/aboutUsData.js
@@ -2,27 +2,27 @@
 export const aboutUsData = [
     {
         name: 'Timo Hyttinen',
-        title: 'UI/UX Designer',
-        description: 'Timo is the CEO of our company. He is responsible for the overall vision and strategy of the company. He has a background in software engineering and has a passion for technology and innovation.',
+        title: 'Designer Goat',
+        description: 'Timo is scrum master and UI/UX designer of our team. He is responsible for UI and some of the visual components. He likes cats, plants and board games.',
     },
     {
         name: 'Roosa Rautio',
-        title: 'UI/UX Designer',
-        description: 'Roosa is the CTO of our company. She is responsible for the technical direction of the company. She has a background in computer science and has a passion for building great products.',
+        title: 'Canva Pro',
+        description: 'Roosa is UI/UX designer of our team. She is responsible for most of the visual components, prototypes and UI. She likes to drink her coffee with oat milk.',
     },
     {
         name: 'Severi Jokelainen',
-        title: 'Lead Developer',
-        description: 'Severi is the COO of our company. He is responsible for the operations of the company. He has a background in business and has a passion for building great teams.',
+        title: 'Frontend Wizard',
+        description: 'Severi is frontend developer of our team. He is responsible for frontend coding for trackers and main functionality of the app. He likes to party and lift heavy weights at the gym.',
     }, 
     {
         name: 'Matti Pitkänen',
-        title: 'Lead Developer',
-        description: 'Matti is the CFO of our company. He is responsible for the financial health of the company. He has a background in finance and has a passion for building great financial systems.',
+        title: 'Backend Captain',
+        description: 'Matti is backend developer of our team. He is responsible for backend coding. He is nerd from the great wilderness of Northern Finland and he is a craft beer enjoyer.',
     },
     {
         name: 'Samuli Ruotsalainen',
-        title: 'Lead Developer',
-        description: 'Samuli is the CMO of our company. He is responsible for the marketing and sales of the company. He has a background in marketing and has a passion for building great brands.',
+        title: 'Authentication Master',
+        description: 'Samuli is backend developer and Firebase administrator of our team. He is responsible for our database and account management. He has serious addiction to Runescape.',
     }
 ];
