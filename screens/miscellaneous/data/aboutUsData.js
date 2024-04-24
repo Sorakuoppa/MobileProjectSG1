8f9a65770fbd1d62ca7657c480c88f57a1c41253
Serveriii <n2jose00@students.oamk.fi>
2024-04-24 10:51:02 +0300

    AboutUs page

---
+++ screens/miscellaneous/data/aboutUsData.js
@@ -0,0 +1,28 @@
+
+export const aboutUsData = [
+    {
+        name: 'Timo Hyttinen',
+        title: 'CEO',
+        description: 'Timo is the CEO of our company. He is responsible for the overall vision and strategy of the company. He has a background in software engineering and has a passion for technology and innovation.',
+    },
+    {
+        name: 'Roosa Rautio',
+        title: 'CTO',
+        description: 'Roosa is the CTO of our company. She is responsible for the technical direction of the company. She has a background in computer science and has a passion for building great products.',
+    },
+    {
+        name: 'Severi Jokelainen',
+        title: 'COO',
+        description: 'Severi is the COO of our company. He is responsible for the operations of the company. He has a background in business and has a passion for building great teams.',
+    }, 
+    {
+        name: 'Matti Pitkänen',
+        title: 'CFO',
+        description: 'Matti is the CFO of our company. He is responsible for the financial health of the company. He has a background in finance and has a passion for building great financial systems.',
+    },
+    {
+        name: 'Samuli Ruotsalainen',
+        title: 'CMO',
+        description: 'Samuli is the CMO of our company. He is responsible for the marketing and sales of the company. He has a background in marketing and has a passion for building great brands.',
+    }
+];
--- screens/miscellaneous/moreAboutUs.js
+++ screens/miscellaneous/moreAboutUs.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { useTheme } from "@react-navigation/native";
+import { aboutUsData } from "./data/aboutUsData";
+import { general } from "../../styles/general";
+
 
 export default function MoreAboutUs({ navigation, route }) {
   const { colors } = useTheme();
   const { name } = route.params;
 
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ color: colors.text }}>More about us</Text>
+    <View style={{ flex: 1, justifyContent: "flex-start", alignItems: "center" }}>
+      <Text style={{...general.title,  color: colors.text }}>More about us</Text>
       <Text style={{ color: colors.text }}>Name: {name}</Text>
       <TouchableOpacity onPress={() => navigation.navigate("Home")}>
         <Text style={{ color: colors.text }}>Go to Home</Text>
