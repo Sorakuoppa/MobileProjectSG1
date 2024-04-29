209f52da35a52d037020b167f498c8ca2dd167c7
saruo <ruotsalainen.samuli@gmail.com>
2024-04-29 17:29:33 +0300

    Working on fixing first time load screen routing

--- components/Contexts/FirstTimeLoadContext.js
+++ components/Contexts/FirstTimeLoadContext.js
@@ -1,11 +1,38 @@
-import React, { createContext, useState, useContext } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 // Create context
 export const FirstTimeLoadContext = createContext(); // Renamed context
 
 // Create provider
 export const FirstTimeLoadProvider = ({ children }) => {
-  const [firstTimeLoaded, setFirstTimeLoaded] = useState(false); // Renamed variable
+  const [firstTimeLoaded, setFirstTimeLoadedState] = useState(false); // Renamed variable
+
+  useEffect(() => {
+    // Load the value of firstTimeLoaded from AsyncStorage
+    const loadFirstTimeLoaded = async () => {
+      try {
+        const value = await AsyncStorage.getItem('firstTimeLoaded');
+        if (value !== null) {
+          setFirstTimeLoadedState(value === 'true');
+        }
+      } catch (error) {
+        console.error('Error loading firstTimeLoaded from AsyncStorage:', error);
+      }
+    };
+    loadFirstTimeLoaded();
+  }, []);
+
+  const setFirstTimeLoaded = async (value) => {
+    try {
+      // Save the value of firstTimeLoaded to AsyncStorage
+      await AsyncStorage.setItem('firstTimeLoaded', value.toString());
+      // Update the context state
+      setFirstTimeLoadedState(value);
+    } catch (error) {
+      console.error('Error saving firstTimeLoaded to AsyncStorage:', error);
+    }
+  };
 
   return (
     <FirstTimeLoadContext.Provider value={{ firstTimeLoaded, setFirstTimeLoaded }}>
